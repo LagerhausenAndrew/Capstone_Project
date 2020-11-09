d96494adcb8215452225bb5ae6978548dd2d7ea2
lagerhausenandrew <lagerhausen.3@gmail.com>
2020-11-09 14:17:36 -0600

    structure update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
 
 // Components
 import Header from './components/header';
 import Footer from './components/footer';
-import HomepageContainer from './components/home_page_container'
-import ProductPageContainer from './components/product_page_container';
-import UserPageContainer from './components/user_page_container';
-import BlogPageContainer from './components/blog_page_container';
-import ContactPageContainer from './components/contact_page_container';
+import OverallContainer from './components/overall_container';
 
 function App() {
   return (
@@ -18,11 +13,7 @@ function App() {
         <Header/>
       </header>
       <main>
-      <Route exact path="/" render={() => <HomepageContainer />} />
-      <Route path="/products" render={() => <ProductPageContainer />} />
-      <Route path="/blog" render={() => <BlogPageContainer />} />
-      <Route path="/contact" render={() => <ContactPageContainer />} />
-      <Route path="/user" render={() => <UserPageContainer />} />
+        <OverallContainer />
       </main>
       <footer>
         <Footer/>
---
+++ src/components/overall_container.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+// Components
+import HomepageContainer from './home_page_container'
+import ProductPageContainer from './product_page_container';
+import UserPageContainer from './user_page_container';
+import BlogPageContainer from './blog_page_container';
+import ContactPageContainer from './contact_page_container';
+
+const OverallContainer = (props) => {
+  return (
+    <div>
+        <Route exact path="/" render={() => <HomepageContainer />} />
+        <Route path="/products" render={() => <ProductPageContainer />} />
+        <Route path="/blog" render={() => <BlogPageContainer />} />
+        <Route path="/contact" render={() => <ContactPageContainer />} />
+        <Route path="/user" render={() => <UserPageContainer />} />
+    </div>
+  );
+}
+
+export default OverallContainer;
