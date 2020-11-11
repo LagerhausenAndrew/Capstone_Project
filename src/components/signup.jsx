9d1eb0deaa6ae921625a5afebd77b55413747579
lagerhausenandrew <lagerhausen.3@gmail.com>
2020-11-11 10:29:38 -0600

    updating login form

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { TextInput, FormField } from 'evergreen-ui';
+
 class SignUpPage extends Component {
   constructor(props) {
     super(props);
@@ -20,37 +22,41 @@ class SignUpPage extends Component {
 
   render() {
     return(
-    <form onSubmit={(e) => this.props.handleSignUp(e, this.state)}>
-        <input
+    <FormField className="signUpForm" onSubmit={(e) => this.props.handleSignUp(e, this.state)}>
+        Name:
+        <TextInput
           type="text"
           name="name"
           placeholder="Name"
           value={this.state.name}
           onChange={this.handleChange}
         />
-        <input
+        Email:
+        <TextInput
           type="text"
           name="email"
           placeholder="Email"
           value={this.state.email}
           onChange={this.handleChange}
         />
-        <input
+        Username:
+        <TextInput
           type="text"
           name="username"
           placeholder="Username"
           value={this.state.username}
           onChange={this.handleChange}
         />
-        <input
+        Password:
+        <TextInput
           type="password"
           name="password"
           placeholder="Password"
           value={this.state.password}
           onChange={this.handleChange}
         />
-        <input type="submit" value="Sign Up" />
-    </form>
+        <input className="submit" type="submit" value="Sign Up" />
+    </FormField>
     )
   }
 }
